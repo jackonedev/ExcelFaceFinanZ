aeaf9425366cbe380f0926179b370644930baca4
AgustinSt1990 <af.stigliano@gmail.com>
2023-02-09 18:05:23 -0300

    FINALIZADO JUEVES, LISTO PARA COMPILAR

--- main_DEMO.py
+++ main_DEMO.py
@@ -1,6 +1,7 @@
 from tabla_seccion_superior import main
 from packpy.elegir_formato import elegir_moneda_sistema
 import time
+import pandas as pd
 
 def buscar_valor(valor, dicc):#TODO: meter dentro de packpy
     for llave in dicc:#TODO: almacenamiento datos es una variable global
@@ -67,7 +68,7 @@ def main2():
         almacenamiento_datos.lista(prefix=mon_sym)
 
     print ('\nFINALIZANDO PARTE 1 DE LA DEMO:')
-    time.sleep(0.4)
+    # time.sleep(0.4)
     time.sleep(0.4)
     print ('\nCHECKEO DEL VALOR DE LA VARIABLE')
     imprimir_variables(almacenamiento_datos.dicto)
@@ -77,41 +78,63 @@ def main2():
     imprimir_variables(almacenamiento_datos.dicture)
     time.sleep(0.4)
     time.sleep(0.4)
-    print ('\nEXPORTANDO EN CSV LA TABLA DE CUOTAS FUTURAS: (no implementado)')
-    #TODO: llevar a pandas.DataFrame y exportar a csv
-    #TODO: si los values son list, y las list tienen mismo len, devolver DataFrame
-    print ('\nARRANCANDO PARTE 2 DE LA DEMO:')
 
 
     ## IMPORTANTE AL LECTOR:
-    """Esta parte fue el comienzo del desarrollo de TODO el código, y hay mucho que se va a implementar a continuación que debe ser removida he implementado dentro de los modulos importados"""
-    from tabla_seccion_inferior import main
-    estructura_alquiler, estructura_compra = main()#TODO: aplicar almacenar datos
+    """Esta parte (tabla_seccion_inferior.py) fue el comienzo del desarrollo de TODO el código, y hay mucho que se va a implementar a continuación que debe ser removida he implementado dentro de los modulos importados"""
     # EJEMPLO: Ahora tengo que actualizar los valor de las estructuras_i, y volver a calcular el ahorro y el pct_Cash_Req/Mes
     # EJEMPLO: Le tengo que agregar "capital_inicial" = desembolso + notariado
+    # EJEMPLO: Faltan de agregar variables como "PLAZO"
 
-    estructura_alquiler['capital_inicial'] = buscar_valor('desembolso', almacenamiento_datos) + buscar_valor('notariado', almacenamiento_datos)
+    exportar_pickle = False
+    if exportar_pickle:
+        import pickle
+        with open('almacenamiento_datos.pickle', 'wb') as handle:
+            pickle.dump(almacenamiento_datos, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+        with open('almacenamiento_datos.pickle', 'rb') as handle:
+            file_loaded = pickle.load(handle)
+
+        # search for type list values in file_loaded.dicto.keys()
+        df = []
+        for llave in file_loaded.dicto:
+            if isinstance(file_loaded.dicto[llave], list):
+                df.append({llave: file_loaded.dicto[llave]})
+                print (f'{llave}: list')
+
+    print ('\nARRANCANDO PARTE 2 DE LA DEMO:')
+
+    from tabla_seccion_inferior import main
+
+
+    estructura_alquiler, estructura_compra = main()
+
+    estructura_alquiler['capital_inicial'] = buscar_valor('desembolso', almacenamiento_datos.dicto) + buscar_valor('notariado', almacenamiento_datos.dicto)
     estructura_compra['capital_inicial'] = estructura_alquiler['capital_inicial']
 
-    label_notariado = buscar_valor('notariado_pct', almacenamiento_datos)
-    label_desembolso = buscar_valor('desembolso_pct', almacenamiento_datos)
+    label_notariado = buscar_valor('notariado_pct', almacenamiento_datos.dicto)
+    label_desembolso = buscar_valor('desembolso_pct', almacenamiento_datos.dicto)
 
-    estructura_compra[f'notariado {label_notariado} %'] = - buscar_valor('notariado', almacenamiento_datos)
-    estructura_compra[f'desembolso {label_desembolso} %'] = - buscar_valor('desembolso', almacenamiento_datos)
-    estructura_alquiler[f'notariado {label_notariado} %'] = 0
-    estructura_alquiler[f'desembolso {label_desembolso} %'] = 0
-    
-    estructura_compra['Hip/Alquiler_Mensual'] = - cuota
-    estructura_compra['Ahorros_Mensuales'] += cuota - estructura_alquiler['Hip/Alquiler_Mensual']
+    label_notariado = f'notariado {label_notariado} %'
+    label_desembolso = f'desembolso {label_desembolso} %'
+
+    estructura_compra[label_notariado] = - buscar_valor('notariado', almacenamiento_datos.dicto)
+    estructura_compra[label_desembolso] = - buscar_valor('desembolso', almacenamiento_datos.dicto)
+    estructura_alquiler[label_notariado] = 0
+    estructura_alquiler[label_desembolso] = 0
+
+    estructura_compra['Hip/Alquiler_Mensual'] = - int(cuota)
+    estructura_compra['Ahorros_Mensuales'] = int(estructura_compra['Sueldo_Mensual'] - cuota - estructura_compra['Gastos_Mantenimiento'] - estructura_compra['Gastos_Suministros'] - estructura_compra['Cash_Lifestyle'])
     estructura_compra['pct_Cash_Req/Mes'] = int((float(estructura_compra['Sueldo_Mensual']) - float(estructura_compra['Ahorros_Mensuales'])) / float(estructura_compra['Sueldo_Mensual']) *100)
     estructura_alquiler['pct_Cash_Req/Mes'] = int(estructura_alquiler['pct_Cash_Req/Mes'] *100)
-    
-    # SUPUESTO: que le podes sacar x4 de interes que cobra el banco #TODO: agregar un input al usuario
-    estructura_compra['interes_anual'] = buscar_valor('tasa_interes', almacenamiento_datos)
-    estructura_alquiler['interes_anual'] = estructura_compra['interes_anual']  * 4
-    estructura_alquiler['valor_casa'] = buscar_valor('valor_casa', almacenamiento_datos)
-    estructura_compra['valor_casa'] = buscar_valor('valor_casa', almacenamiento_datos)
 
+    # SUPUESTO: que le podes sacar x4 de interes que cobra el banco #TODO: agregar un input al usuario
+    estructura_compra['interes_anual'] = buscar_valor('tasa_interes', almacenamiento_datos.dicto)
+    estructura_alquiler['interes_anual'] = estructura_compra['interes_anual']  * 4
+    estructura_alquiler['valor_casa'] = buscar_valor('valor_casa', almacenamiento_datos.dicto)
+    estructura_compra['valor_casa'] = buscar_valor('valor_casa', almacenamiento_datos.dicto)
+
+    #TODO: REDONDEAR PCT_CASH_REQ_MES
 
     time.sleep(1)
     print ('\nCHECKEO DEL COMPRAR')
@@ -123,7 +146,72 @@ def main2():
         print (elemento, estructura_alquiler[elemento])
     time.sleep(1)
 
-    # CREANDO DATAFRAME CON LOS RESULTADOS
+    # CREANDO DATAFRAMES
+    hipoteca = pd.Series(estructura_compra.values(), estructura_compra.keys())
+    alquiler = pd.Series(estructura_alquiler.values(), estructura_alquiler.keys())
 
-     # EXPORTANDO TODO EN EXCEL
-main2()
+    tabla_input = pd.DataFrame({
+        'Hipoteca': hipoteca,
+        'Alquiler': alquiler
+    })
+
+    tabla_input.loc['interes_anual','Hipoteca'] *= -1
+    orden_variables = ['valor_casa', 'capital_inicial', 
+    label_desembolso, label_notariado, 'interes_anual',
+    'Sueldo_Mensual', 'Hip/Alquiler_Mensual', 'Ahorros_Mensuales',
+    'Gastos_Mantenimiento', 'Gastos_Suministros', 'pct_Cash_Req/Mes', 'Cash_Lifestyle'
+    ]
+
+    tabla_input = tabla_input.reindex(index=orden_variables)
+    new_rows = []
+    for row in list(tabla_input.index):
+        new_rows.append(row.replace('_',' ').title())
+
+    tabla_input.to_excel('Input_ZARTEX_TOOL.xlsx')
+
+
+    pago_intereses = buscar_valor('pago_interes', almacenamiento_datos.dicto)
+    amortizacion = buscar_valor('amortizacion', almacenamiento_datos.dicto)
+    saldo = buscar_valor('saldo', almacenamiento_datos.dicto)
+
+    ahorro_mensual_hipoteca = tabla_input.loc['Ahorros Mensuales','Hipoteca']
+    ahorro_mensual_alquiler = tabla_input.loc['Ahorros Mensuales','Alquiler']
+
+    lista_amha = []
+    lista_amaa = []
+    lista_cuota = []
+
+    diccionario_DEMO = {}
+    for i in range(0, len(pago_intereses)+1):
+        if i == 0:
+            print ('Deuda total inicial: € {}'.format(almacenamiento_datos.formato_numerico(buscar_valor('valor_casa', almacenamiento_datos.dicto) - buscar_valor('desembolso', almacenamiento_datos.dicto))))
+            print ('Cuota mensual hipoteca: € {}'.format(almacenamiento_datos.formato_numerico(buscar_valor('cuota', almacenamiento_datos.dicto))))
+            continue
+        
+        numero_cuota = str(i)
+        if i < 10:
+            numero_cuota = '0' + str(i)
+        lista_cuota.append('Cuota nº ' + numero_cuota)
+        lista_amaa.append('€ ' + almacenamiento_datos.formato_numerico(int(ahorro_mensual_alquiler*i)))
+        lista_amha.append('€ ' + almacenamiento_datos.formato_numerico(int(ahorro_mensual_hipoteca*i)))
+
+
+    diccionario_DEMO['Cuota'] = lista_cuota
+    diccionario_DEMO['Ahorro "Alquiler"'] = lista_amaa
+    diccionario_DEMO['Ahorro "Hipoteca"'] = lista_amha
+    diccionario_DEMO['Pago de Interés'] = pago_intereses
+    diccionario_DEMO['Ahorro CASA'] = amortizacion
+    diccionario_DEMO['Deuda Restante'] = saldo
+
+
+    df_columns = diccionario_DEMO.keys()
+    df_index = range(len(diccionario_DEMO['Cuota']))
+    df = pd.DataFrame(diccionario_DEMO, index=df_index, columns=df_columns)
+
+    df.to_csv('TABLA_RESUMEN_CUOTAS_FUTURAS.csv')
+
+
+if __name__ == '__main__':
+    main2()
+    print ('Muchas gracias por usar el programa.')
+    print ('Programa finalizado exitosamente')
